efe4b50a85453595815975ef25a7f60dfd5fb7e5
Mathewsmusukuma <sikaili99@gmail.com>
2021-02-09 18:29:07 +0200

    Remove the layouts as the app will use reactjs for the front-end

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Expresso
-Express js boilerplate for a quick start of the full stack app development
+Express js boilerplate for a quick start of the full stack app development.
+
+Backend for the Expresso app
--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const exphbs  = require('express-handlebars');
- 
+
 const app = express();
  
 // parse application/x-www-form-urlencoded
@@ -10,26 +9,18 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-//Express handlebars view engine
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
-
 //Rootes here
 
 app.get('/', (req, res)=>{
-    res.render("index");
+    res.send({"message": "Welcome to Expresso!"})
 });
-
 
 app.get('/api/users/user/:id', (req, res)=>{
 	const userId = req.params.id;
     res.status(200).send(`User with ${userId} has been found!`);
 });
         
-
-
 //App port settings here
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`The app is listening on ${port}`))
-
